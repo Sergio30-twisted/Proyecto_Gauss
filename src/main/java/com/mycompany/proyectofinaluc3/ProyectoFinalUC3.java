dfe2e7d5f9c10880b18f850912c05aaf4f9d7e5d
Sergio30-twisted <padparadscha30@gmail.com>
2025-10-20 20:24:34 -0700

    Prueba 1

---
+++ src/main/java/com/mycompany/proyectofinaluc3/ProyectoFinalUC3.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectofinaluc3;
+
+/**
+ *
+ * @author Sergio Aboytia
+ */
+public class ProyectoFinalUC3 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
